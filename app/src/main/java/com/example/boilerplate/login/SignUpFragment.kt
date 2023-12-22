56fc417b76d1df09a2c8d3063947cba3b68a9a4d
faryz-coder <faryz.coder@gmail.com>
2023-12-23 02:50:06 +0800

    integrate date picker

--- app/src/main/java/com/example/boilerplate/login/SignUpFragment.kt
+++ app/src/main/java/com/example/boilerplate/login/SignUpFragment.kt
@@ -1,13 +1,18 @@
 package com.example.boilerplate.login
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.example.boilerplate.R
 import com.example.boilerplate.databinding.FragmentSignupBinding
+import com.google.android.material.datepicker.MaterialDatePicker
+import java.util.Calendar
+import kotlin.time.Duration.Companion.days
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -19,13 +24,18 @@ class SignUpFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+    private lateinit var datePicker: MaterialDatePicker<Long>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
 
         _binding = FragmentSignupBinding.inflate(inflater, container, false)
+
+        instantiateDatePicker()
+
+
         return binding.root
 
     }
@@ -36,6 +46,37 @@ class SignUpFragment : Fragment() {
 //        binding.buttonSecond.setOnClickListener {
 //            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
 //        }
+
+        binding.btnSelectDate.setOnClickListener {
+            datePicker.show(requireActivity().supportFragmentManager, "dateOfBirth")
+        }
+
+    }
+
+    private fun instantiateDatePicker() {
+        datePicker =
+            MaterialDatePicker.Builder.datePicker()
+                .setTitleText("Select Date Of Birth")
+                .setSelection(MaterialDatePicker.todayInUtcMilliseconds())
+                .build()
+
+        datePicker.addOnPositiveButtonClickListener {
+            val selectedDate = formatDate(it)
+            Log.d("Bmh","instantiateDatePicker: it::$it, selection::${selectedDate}")
+            // Set Date to Button Text
+            binding.btnSelectDate.text = selectedDate
+        }
+    }
+
+    private fun formatDate(milliseconds: Long): String {
+        val calendar = Calendar.getInstance()
+        calendar.timeInMillis = milliseconds
+
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+        val month = calendar.get(Calendar.MONTH) + 1 // Month is zero-based
+        val year = calendar.get(Calendar.YEAR)
+
+        return "$day/$month/$year"
     }
 
     override fun onDestroyView() {
