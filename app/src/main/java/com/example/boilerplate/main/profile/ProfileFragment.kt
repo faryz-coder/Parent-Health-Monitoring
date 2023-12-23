899a13f4d6adb0abf316dbde509d96a242822a57
faryz-coder <faryz.coder@gmail.com>
2023-12-23 09:35:25 +0800

    profile - select image

--- app/src/main/java/com/example/boilerplate/main/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/boilerplate/main/profile/ProfileFragment.kt
@@ -1,9 +1,12 @@
 package com.example.boilerplate.main.profile
 
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import com.example.boilerplate.databinding.FragmentProfileBinding
 
@@ -11,13 +14,25 @@ class ProfileFragment : Fragment(){
     private var _binding: FragmentProfileBinding? = null
 
     private val binding get() = _binding!!
+    private var updateImg: Uri? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentProfileBinding.inflate(inflater, container,false)
+
+        val pickMedia = registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
+            if (uri != null) {
+                binding.profileImage.setImageURI(uri)
+                updateImg = uri
+            }
+        }
+
+        binding.btnSelectImage.setOnClickListener {
+            pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+        }
 
         return binding.root
     }
