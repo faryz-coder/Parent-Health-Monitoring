b2f52c45c9ea3415628de68afb251c7776a0dcdb
faryz-coder <faryz.coder@gmail.com>
2023-12-24 02:05:09 +0800

    calculate bmi - update logic

--- app/src/main/java/com/example/boilerplate/main/bmi/BmiCalculatorFragment.kt
+++ app/src/main/java/com/example/boilerplate/main/bmi/BmiCalculatorFragment.kt
@@ -1,16 +1,23 @@
 package com.example.boilerplate.main.bmi
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.boilerplate.R
 import com.example.boilerplate.databinding.FragmentBmiBinding
+import com.google.android.material.slider.Slider
+import kotlin.math.sqrt
 
-class BmiCalculatorFragment : Fragment() {
+class BmiCalculatorFragment : Fragment(), View.OnClickListener {
     private var _binding: FragmentBmiBinding? = null
 
     private val binding get() = _binding!!
+    private var weight = 0F
+    private var height: Float = 0F
+    private var age = 0F
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -18,6 +25,89 @@ class BmiCalculatorFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentBmiBinding.inflate(inflater, container, false)
+
+        binding.bmiHeight.text = getString(R.string.input_cm, height.toString())
+
+
+        binding.heightBar.addOnChangeListener { _, value, _ ->
+            // Responds to when slider's value is changed
+            Log.d("BmiCalculatorFragment", "value: $value")
+            height = value
+            binding.bmiHeight.text = getString(R.string.input_cm, height.toString())
+        }
+
+        reset()
+
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.bmiWeightMinus.setOnClickListener(this)
+        binding.bmiWeightAdd.setOnClickListener(this)
+        binding.bmiAgeAdd.setOnClickListener(this)
+        binding.bmiAgeMinus.setOnClickListener(this)
+        binding.btnCalculateBmi.setOnClickListener(this)
+        binding.btnResetBmi.setOnClickListener(this)
+    }
+
+    override fun onClick(btn: View) {
+        when (btn.id) {
+            binding.bmiWeightMinus.id -> setWeight(false)
+            binding.bmiWeightAdd.id -> setWeight(true)
+            binding.bmiAgeMinus.id -> setAge(false)
+            binding.bmiAgeAdd.id -> setAge(true)
+            binding.btnResetBmi.id -> reset()
+            binding.btnCalculateBmi.id -> calculateBmi()
+        }
+    }
+
+    private fun calculateBmi() {
+        val bmi = height / (sqrt(weight))
+
+        if (bmi < 16.0F) {
+            binding.bmiStatus.text = getString(R.string.you_are_severely_underweight)
+        } else if (bmi < 18.5F ) {
+            binding.bmiStatus.text = getString(R.string.you_are_underweight)
+        } else if (bmi < 25.0F) {
+            binding.bmiStatus.text = getString(R.string.you_are_normal)
+        } else if (bmi < 30.0F) {
+            binding.bmiStatus.text = getString(R.string.you_are_overweight)
+        } else if (bmi < 35.0F) {
+            binding.bmiStatus.text = getString(R.string.you_are_moderately_obese)
+        } else if (bmi > 35.0F) {
+            binding.bmiStatus.text = getString(R.string.you_are_severely_obese)
+        }
+    }
+
+    private fun setAge(input: Boolean) {
+        if (input) {
+            age += 1F
+            binding.bmiAge.text = getString(R.string.input_years, age.toString())
+        } else {
+            age -= 1F
+            binding.bmiAge.text = getString(R.string.input_years, age.toString())
+        }
+    }
+
+    private fun setWeight(input: Boolean) {
+        if (input) {
+            weight += 1F
+            binding.bmiWeight.text = getString(R.string.input_kg, weight.toString())
+        } else {
+            weight -= 1F
+            binding.bmiWeight.text = getString(R.string.input_kg, weight.toString())
+        }
+    }
+
+    private fun reset() {
+        age = 0F
+        weight = 0F
+        height = 0F
+
+        binding.bmiWeight.text = getString(R.string.input_kg, "0")
+        binding.bmiAge.text = getString(R.string.input_years, "0")
+        binding.heightBar.value = 0F
     }
 }
