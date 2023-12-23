444b19046280c59cc01eb5e66b13f19bd78d07b7
faryz-coder <faryz.coder@gmail.com>
2023-12-24 06:35:15 +0800

    connect to firestore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,8 @@ dependencies {
     // Add the dependency for the Firebase Authentication library
     // When using the BoM, you don't specify versions in Firebase library dependencies
     implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-firestore")
+
     //
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
